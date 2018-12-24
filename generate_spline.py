6a36c56e63d8ae4541ba202a292feeacdd95c487
brendon-ai <brendon-m@outlook.com>
2018-12-23 19:21:30 -0500

    Rough attempt at loss optimization

--- generate_spline.py
+++ generate_spline.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 """Generate a spline to avoid a set of obstacles in the 2D plane"""
 
+from autograd import grad
+import autograd.numpy as np
 import matplotlib.pyplot as plt
-import numpy as np
 from scipy.interpolate import splrep, splev
+from scipy.optimize import minimize, basinhopping
 
-# DEFINITIONS
-# Create a range of Y axis values to plot on
-y = np.arange(-10, 10, 0.05)
+# Create a range of fairly widely spaced Y axis values to optimize
+y = np.arange(-10, 10, 0.25)
 # Define the edges of the road in terms of linear functions of the form x=my+b (all units are in meters)
 left_edge = 0.02 * y ** 2 + 0.5 * y + 2
 right_edge = 0.02 * y ** 2 + 0.5 * y
 # Define a set of obstacles present within the road
-obstacles = [(-1.5, -5), (1, -1), (5, -1), (4, 5), (4.1, 5), (1.3, -0.4)]
-
-# COMPUTATIONS
+obstacles = [(-1.6, -5), (1.1, -1), (5.1, -1), (4.1, 5), (4.1, 5), (1.3, -0.4)]
+# Get the X and Y positions of the obstacles individually
+obstacles_x, obstacles_y = [np.array(value_list) for value_list in zip(*obstacles)]
 # The optimal free path (assuming no obstacles) should be the average of the left and right edges
 free_path = (left_edge + right_edge) / 2
+
+
+def path_loss(path):
+    """Given a set of X values corresponding to the predefined Y range, return a loss that optimizes distance from obstacles as well as the ideal path"""
+    # Get the mean squared error of the path from the constant free path
+    path_mean_squared_error = np.mean(np.square(free_path - path))
+    # Create a list containing things to add up to produce the final loss
+    loss_values = [path_mean_squared_error]
+    # Iterate over the obstacles, adding to the loss
+    for obstacle_x, obstacle_y in obstacles:
+        # Get the Pythagorean distance from each point on the path to this obstacle
+        distances = np.sqrt(np.square(obstacle_x - path) + np.square(obstacle_y - y))
+        # Invert all of these distances so closer is worse, multiply them by a constant, and add them to the loss
+        loss_values.append(np.mean(1 / distances) * 1)
+    # Return the aggregated loss
+    return np.sum(loss_values)
+
+
+# Minimize the loss to produce an optimal path
+path = minimize(path_loss, x0=np.zeros(shape=len(y)), method='TNC', jac=grad(path_loss)).x
+print(path)
+
+
 # Initialize a list of spline points to add to the winding path
 spline_points = []
 # Next, we want to create a series of points that a spline can be fit to, which avoids the obstacles with the most room to spare
@@ -60,9 +84,8 @@ spline = splrep(spline_points_y, spline_points_x)
 # Evaluate the spline on the full Y range
 spline_x = splev(y, spline)
 
-# DISPLAY
 # Display the relevant edges and paths
-plt.plot(left_edge, y, right_edge, y, spline_x, y)
+plt.plot(left_edge, y, right_edge, y, spline_x, y, path, y)
 # Scatter plot the obstacles
 plt.scatter(*zip(*obstacles))
 # Show the combined plot on screen
