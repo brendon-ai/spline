003dc2a46ad4680e4987b11443048c5b59057807
brendon-ai <brendon-m@outlook.com>
2018-12-24 16:24:33 -0500

    Include actual path for experimental purposes

--- generate_spline.py
+++ generate_spline.py
@@ -3,21 +3,9 @@
 
 from autograd import grad
 import autograd.numpy as np
-import matplotlib.pyplot as plt
 from scipy.interpolate import splrep, splev
 from scipy.optimize import minimize, basinhopping
-
-# Create a range of fairly widely spaced Y axis values to optimize
-y = np.arange(-10, 2, 1)
-# Define the edges of the road in terms of linear functions of the form x=my+b (all units are in meters)
-left_edge = 0.02 * y ** 2 + 0.5 * y + 5
-right_edge = 0.02 * y ** 2 + 0.5 * y
-# Define a set of obstacles present within the road
-obstacles = [(-1.6, -5), (0.6, -2), (5.1, -1), (0.8, -1.4), ((1.5, -3))]
-# Get the X and Y positions of the obstacles individually
-obstacles_x, obstacles_y = [np.array(value_list) for value_list in zip(*obstacles)]
-# The optimal free path (assuming no obstacles) should be the average of the left and right edges
-free_path = (left_edge + right_edge) / 2
+import zmq
 
 
 def path_loss(path):
@@ -40,17 +28,34 @@ def path_loss(path):
     return np.sum(loss_values)
 
 
-# Minimize the loss to produce an optimal path
-path = minimize(path_loss, x0=free_path, method='TNC', jac=grad(path_loss)).x
-# Fit a spline to the points (switching X and Y because Y is increasing, not X)
-spline = splrep(y, path)
-# Evaluate the spline on a denser Y range
-dense_y = np.arange(-10, 2, 0.05)
-spline_x = splev(dense_y, spline)
+# Connect to the simulation using ZeroMQ
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind('tcp://*:5556')
+# Infinite loop during which we receive packets from the Unity simulation
+while True:
+    # Get a message from the simulation
+    message = socket.recv()
+    print(message)
+    # Create a range of fairly widely spaced Y axis values to optimize
+    y = np.arange(-10, 2, 1)
+    # Define the edges of the road in terms of linear functions of the form x=my+b (all units are in meters)
+    left_edge = 0.02 * y ** 2 + 0.5 * y + 5
+    right_edge = 0.02 * y ** 2 + 0.5 * y
+    # Define a set of obstacles present within the road
+    obstacles = [(-1.6, -5), (0.6, -2), (5.1, -1), (0.8, -1.4), ((1.5, -3))]
+    # Get the X and Y positions of the obstacles individually
+    obstacles_x, obstacles_y = [np.array(value_list) for value_list in zip(*obstacles)]
+    # The optimal free path (assuming no obstacles) should be the average of the left and right edges
+    free_path = (left_edge + right_edge) / 2
+    # Minimize the loss to produce an optimal path
+    path = minimize(path_loss, x0=free_path, method='TNC', jac=grad(path_loss)).x
+    # Fit a spline to the points (switching X and Y because Y is increasing, not X)
+    spline = splrep(y, path)
+    # Evaluate the spline on a denser Y range
+    dense_y = np.arange(-10, 2, 0.05)
+    spline_x = splev(dense_y, spline)
+    # Send the resulting list of points, formatted as a JSON list
+    # socket.send_json(list(spline_x))
+    socket.send_json('{"pathPoints":[{"x":4.880000114440918,"y":0.09000000357627869},{"x":8.0600004196167,"y":0.09000000357627869},{"x":11.229999542236329,"y":0.7799999713897705},{"x":14.210000038146973,"y":2.4800000190734865},{"x":16.559999465942384,"y":4.670000076293945},{"x":18.229999542236329,"y":6.900000095367432},{"x":19.110000610351564,"y":9.579999923706055},{"x":18.719999313354493,"y":12.329999923706055},{"x":17.280000686645509,"y":14.90999984741211},{"x":15.770000457763672,"y":17.229999542236329},{"x":15.34000015258789,"y":20.040000915527345},{"x":15.800000190734864,"y":22.979999542236329},{"x":17.299999237060548,"y":25.200000762939454},{"x":19.360000610351564,"y":26.969999313354493},{"x":22.329998016357423,"y":27.40999984741211},{"x":25.509998321533204,"y":27.40999984741211},{"x":28.67999839782715,"y":28.100000381469728},{"x":31.65999984741211,"y":29.799999237060548},{"x":34.0099983215332,"y":31.98999786376953},{"x":35.679996490478519,"y":34.220001220703128},{"x":36.560001373291019,"y":36.900001525878909},{"x":36.16999816894531,"y":39.650001525878909},{"x":34.72999954223633,"y":42.22999954223633},{"x":33.21999740600586,"y":44.54999923706055},{"x":32.78999710083008,"y":47.36000061035156},{"x":33.25,"y":50.29999923706055},{"x":34.749996185302737,"y":52.52000045776367},{"x":36.80999755859375,"y":54.290000915527347}]}')
 
-# Display the relevant edges and paths
-plt.plot(left_edge, y, right_edge, y, spline_x, dense_y, free_path, y)
-# Scatter plot the obstacles
-plt.scatter(*zip(*obstacles))
-# Show the combined plot on screen
-plt.show()
