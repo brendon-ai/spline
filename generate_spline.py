fbf2064dbe882451756a708b2bebf20796c3ac8b
brendon-ai <brendon-m@outlook.com>
2019-01-06 14:18:50 -0500

    Average road edges to plan path

--- generate_spline.py
+++ generate_spline.py
@@ -8,8 +8,10 @@ from scipy.optimize import minimize, basinhopping
 import zmq
 
 
-def path_loss(path, x, free):
+def path_loss(path, x, free, points):
     """Given a set of Y values corresponding to the predefined X range, return a loss that optimizes distance from obstacles as well as the ideal path"""
+    # Extract the points from the list
+    right0, left0, right1, left1 = points
     # Get the mean squared error of the path from the constant free path
     path_mean_squared_error = np.mean(np.square(free - path))
     # Create a list containing things to add up to produce the final loss
@@ -18,14 +20,14 @@ def path_loss(path, x, free):
     loss_values.append(100_000 * np.square(path[0]))
     # Iterate over the path, calculating differences from one point to the next so the derivative is incorporated into the loss
     for point_index in range(len(path) - 1):
-        # Add the squared derivative to the loss list
-        loss_values.append(np.square((path[point_index] - path[point_index + 1]) / (x[point_index] - x[point_index + 1])) * 25 / (len(path) - 1))
+        # Add the squared derivative to the loss list (multiplied by a constant and divided by the number of derivatives)
+        loss_values.append(np.square((path[point_index] - path[point_index + 1]) / (x[point_index] - x[point_index + 1])) * 10 / (len(path) - 1))
     # Iterate over the obstacles, adding to the loss
     for obstacle_x, obstacle_y in obstacles:
         # Get the Pythagorean distance from each point on the path to this obstacle
         distances = np.sqrt(np.square(obstacle_x - x) + np.square(obstacle_y - path))
         # Invert all of these distances so closer is worse, multiply them by a constant, and add them to the loss
-        loss_values.append(np.mean((4 / distances) ** 2) * 60)
+        loss_values.append(np.mean((4 / distances) ** 2) * 20)
     # Return the aggregated loss
     return np.sum(loss_values)
 
@@ -44,10 +46,12 @@ while True:
     obstacles_x, obstacles_y = [np.array(value_list) for value_list in zip(*obstacles)]
     # Create a range of fairly widely spaced X axis values to optimize
     x = np.arange(0, 40, 2)
-    # The optimal free path (assuming no obstacles) should be a line to the provided endpoint
-    end_point_x = message['endPoint']['x']
-    end_point_y = message['endPoint']['y']
-    free_path_line = np.polyfit([0, end_point_x], [0, end_point_y], deg=1)
+    # Get the four points marking out the road edges
+    right0, left0, right1, left1 = [np.array([message[point_name]['x'], message[point_name]['y']]) for point_name in ['right0', 'left0', 'right1', 'left1']]
+    # The optimal free path (assuming no obstacles) should be the average of the two road edges
+    average0 = (right0 + left0) / 2
+    average1 = (right1 + left1) / 2
+    free_path_line = np.polyfit([average0[0], average1[0]], [average0[1], average1[1]], deg=1)
     free_path = np.poly1d(free_path_line)(x)
     # Try a few distortions of the main free path, and choose the one that produces the best loss
     paths = []
@@ -55,10 +59,10 @@ while True:
         # Convert the line to an actual path
         possible_free_path = np.poly1d(possible_free_path_line)(x)
         # Minimize the loss to produce an optimal path
-        possible_path = minimize(path_loss, x0=possible_free_path, args=(x, free_path), method='TNC', jac=grad(path_loss)).x
+        possible_path = minimize(path_loss, x0=possible_free_path, args=(x, free_path, (right0, left0, right1, left1)), method='TNC', jac=grad(path_loss)).x
         paths.append(possible_path)
     # Choose the path with the lowest loss
-    path = min(paths, key=lambda p: path_loss(p, x, free_path))
+    path = min(paths, key=lambda p: path_loss(p, x, free_path, (right0, left0, right1, left1)))
     # Fit a spline to the points
     spline = splrep(x, path)
     # Evaluate the spline on a denser X range
