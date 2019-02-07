5490003217013237423232672de2a4b76e29e89f
brendon-ai <brendon-m@outlook.com>
2019-02-07 15:50:23 -0500

    Created visual appearance of bike

--- example.py
+++ example.py
@@ -5,12 +5,12 @@ import time
 import pybullet_data
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
-p.setGravity(0, 0, -10)
+p.setGravity(0, 0, 0)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
-boxId = p.loadURDF("/home/brendonm/test.urdf", cubeStartPos, cubeStartOrientation)
-for i in range(100):
+boxId = p.loadURDF("bike.xml", cubeStartPos, cubeStartOrientation)
+for i in range(1000):
     p.stepSimulation()
     time.sleep(1./240.)
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
