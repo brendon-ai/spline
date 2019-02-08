a0439a110c0ec8c8c4324fdad84b3839bdf37be3
brendon-ai <brendon-m@outlook.com>
2019-02-08 17:54:17 -0500

    Successful connection between physics and controls

--- controller.py
+++ controller.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env python3
 """Control the simulated vehicle's stability and movement"""
 
 import time
 
 import control
 import numpy as np
-import zmq
 
 # Fundamental constants of the vehicle and the environment
-WHEELBASE = 2
-CENTER_OF_MASS_HEIGHT = 1
+WHEELBASE = 1.5
+CENTER_OF_MASS_HEIGHT = 0.3
 GRAVITY = 9.81
 MASS = 16
-WHEEL_RADIUS = 0.375
+WHEEL_RADIUS = 0.15
+
+# Store the last time the main loop was run
+last_time = time.time()
+# Store the idealized orthogonal speed over time
+idealized_orthogonal_speed = 0
 
 # Balancing state space control matrices (linearized)
 # States: [tilt, tilt speed, orthogonal speed]
@@ -22,12 +25,11 @@ A = np.array([[0, 1, 0], [GRAVITY / CENTER_OF_MASS_HEIGHT, 0, 0], [0, 0, 0]])
 # Input matrix (defines modification of states based on inputs)
 B = np.array([[0], [-1 / CENTER_OF_MASS_HEIGHT], [1 / MASS]])
 # Loss matrix for states
-Q = 10 * np.diag([1 / (0.3 ** 2), 1 / (2 ** 2), 1 / (5 ** 2)])
+Q = 1 * np.diag([1 / (0.1 ** 2), 1 / (2 ** 2), 1 / (2 ** 2)])
 # Loss matrix for inputs
-R = np.array([[1 / (10 ** 2)]])
+R = np.array([[1 / (40 ** 2)]])
 # Calculate LQR optimal control policy
 K, _, _ = control.lqr(A, B, Q, R)
-print(K)
 
 
 def calculate_wheel_velocity_vectors(x_speed, y_speed, heading_speed, heading):
@@ -48,43 +50,28 @@ def cartesian_to_polar_velocity(x_speed, y_speed):
     return angle, total_speed
 
 
-# Connect to the simulation using ZeroMQ
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind('tcp://*:5556')
-# Store the last time this loop started
-last_time = time.time()
-# Store the idealized orthogonal speed over time
-idealized_orthogonal_speed = 0
-last = 0
-# Infinite loop during which we receive packets from the Unity simulation
-while True:
-    # Get a message from the simulation
-    message = socket.recv_json()
+def control_vehicle(x_pos, y_pos, x_speed, y_speed, tilt, tilt_speed, current_angle_front, current_angle_back):
+    """Given commands and state values, calculate the controls to use for the vehicle"""
     # Get the delta time since the last run of this loop
     current_time = time.time()
+    global last_time
     delta_time = current_time - last_time
     # Store the current time for next run
     last_time = current_time
-    # Take the obstacles from the message
-    obstacles = [(vector['x'], vector['y']) for vector in message['obstacles']]
     # Get the state vector to run the balancing state space controller
-    state_vector = np.array([message['tilt'], message['tiltDerivative'], message['yDerivative']])
+    state_vector = np.array([tilt, tilt_speed, y_speed])
     # Get the reference vector, which should encourage 0 tilt with 0 change, and the desired orthogonal speed
-    reference_vector = np.array([0, 0, message['xCommand'] * 5])
+    reference_vector = np.array([0, 0, 0])
     # Run the state space controller to get the desired orthogonal acceleration
     orthogonal_acceleration = np.matmul(K, reference_vector - state_vector).tolist()[0]
     # Get the desired speed by adding the acceleration multiplied by delta time to the current speed
+    global idealized_orthogonal_speed
     idealized_orthogonal_speed += (orthogonal_acceleration * delta_time)
     # Calculate the wheel velocities needed to satisfy the direction commands
     x_speed_front, x_speed_back, y_speed_front, y_speed_back = calculate_wheel_velocity_vectors(
-        idealized_orthogonal_speed, message['yCommand'], message['headingCommand'], 0)
+        idealized_orthogonal_speed, 0, 0, 0)
     # Calculate the wheel directions and speeds needed to create these velocities
     angle_front, total_speed_front = cartesian_to_polar_velocity(x_speed_front, y_speed_front)
     angle_back, total_speed_back = cartesian_to_polar_velocity(x_speed_back, y_speed_back)
-    if angle_front != last:
-        last = angle_front
-        print(angle_front, total_speed_front, angle_back, total_speed_back)
-    # Send the chosen angles and speeds to the simulation
-    wrapper = {'frontAngle': angle_front, 'backAngle': angle_back, 'frontSpeed': total_speed_front, 'backSpeed': total_speed_back}
-    socket.send_json(wrapper)
+    # Return the chosen angles and speeds
+    return angle_front, angle_back, total_speed_front, total_speed_back
---
+++ simulation.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Simulate an environment for testing of control algorithms"""
+
+import time
+
+import pybullet_data
+import pybullet as p
+
+from controller import control_vehicle
+
+# Constants for the IDs of various joints
+FRONT_SWERVE, FRONT_DRIVE, BACK_SWERVE, BACK_DRIVE = range(4)
+# Time step length
+DELTA_TIME = 1 / 240
+
+# Create a client and connect to the GUI
+client = p.connect(p.GUI)
+# Enable loading of models included by default (the plane)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+# Configure the basic parameters and load the plane
+p.setGravity(0, 0, -9.81)
+plane_id = p.loadURDF('plane.urdf')
+# Load the bike and drop it into the scene
+start_position = [0, 0, 1]
+start_orientation = p.getQuaternionFromEuler([0, 0, 0])
+bike_id = p.loadURDF("bike.xml", start_position, start_orientation)
+# Remember the last tilt so we can calculate the derivative
+last_tilt = 0
+# Run the simulation for a defined number of steps
+for i in range(100_000):
+    # Get the current state of the bike
+    bike_position, bike_orientation_quaternion = p.getBasePositionAndOrientation(bike_id)
+    # Specifically, we want the X and Y positions
+    x_pos, y_pos, _ = bike_position
+    # Convert the orientation to Euler angles, and get the tilt angle
+    tilt, _, _ = p.getEulerFromQuaternion(bike_orientation_quaternion)
+    # Get the current velocity of the bike (positional only)
+    bike_position_speed, _ = p.getBaseVelocity(bike_id)
+    # We want the X and Y speeds
+    x_speed, y_speed, _ = bike_position_speed
+    # Calculate the tilt speed numerically
+    tilt_speed = (tilt - last_tilt) / DELTA_TIME
+    # Update the previous tilt value
+    last_tilt = tilt
+    # Get the current angles of the swerve joints
+    current_angle_front, _, _, _ = p.getJointState(bike_id, FRONT_SWERVE)
+    current_angle_back, _, _, _ = p.getJointState(bike_id, BACK_SWERVE)
+    # Get the desired control values according to the current states
+    angle_front, angle_back, total_speed_front, total_speed_back = control_vehicle(x_pos, y_pos, x_speed, y_speed, tilt, tilt_speed, current_angle_front, current_angle_back)
+    # Set the desired angles of the swerve motors
+    p.setJointMotorControlArray(bodyUniqueId=bike_id, jointIndices=[FRONT_SWERVE, BACK_SWERVE], controlMode=p.POSITION_CONTROL, targetPositions=[angle_front, angle_back], forces=[10] * 2)
+    # Set the desired speeds of the drive motors
+    p.setJointMotorControlArray(bodyUniqueId=bike_id, jointIndices=[FRONT_DRIVE, BACK_DRIVE], controlMode=p.VELOCITY_CONTROL, targetVelocities=[total_speed_front, total_speed_back], forces=[10] * 2)
+    # Step the physics simulation
+    p.stepSimulation()
+    # Wait a small amount of time before moving on to the next step
+    time.sleep(DELTA_TIME)
+# Disconnect from the server at the very end
+p.disconnect()
