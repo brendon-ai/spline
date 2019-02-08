c58553a5a7e59efe4dda10be7f55904169d317a7
brendon-ai <brendon-m@outlook.com>
2019-02-08 10:51:53 -0500

    Fixed wheel joints

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import time
 import pybullet_data
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
-p.setGravity(0, 0, -10)
+p.setGravity(0, 0, -9.81)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
